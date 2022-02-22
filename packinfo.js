99517640e59212d0b40be0aea109e7638409c9f4
Lagicrus <Lagicrus@users.noreply.github.com>
2022-02-22 15:47:14 +0000

    Add medication pack info

---
+++ packinfo.js
@@ -0,0 +1,38 @@
+const packInfo = {
+    Amoxicillin: {
+        strength: "250mg",
+        pack_size: 20
+    },
+    Codeine: {
+        strength: "30mg",
+        pack_size: 10
+    },
+    Diclofenac: {
+        strength: "250mg",
+        pack_size: 25
+    },
+    Ibuprofen: {
+        strength: "500mg",
+        pack_size: 50
+    },
+    Paracetamol: {
+        strength: "500mg",
+        pack_size: 50
+    },
+    Simvastatin: {
+        strength: "10mg",
+        pack_size: 10
+    },
+    Tramadol: {
+        strength: "50mg",
+        pack_size: 100
+    },
+    Warfarin: {
+        strength: "3mg",
+        pack_size: 50
+    }
+}
+
+module.exports = {
+    packInfo
+};
